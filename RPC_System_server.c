16e6458e9092c4001b06ebaed15e47762df9bc30
Tessa Morgan <tessa.morgan870@gmail.com>
2024-10-02 21:30:19 -0500

    compile error

--- RPC_System_server.c
+++ RPC_System_server.c
@@ -14,7 +14,7 @@ void *append_1_svc(ordered *argp, struct svc_req *rqstp)
 	int i=0, j=0;
 
 	list.ordered_val = realloc(list.ordered_val, 
-						((list.ordered_len * sizeof(int)) + (argp->ordered_val * sizeof(int)) + 1));
+						((list.ordered_len * sizeof(int)) + (argp->ordered_len * sizeof(int)) + 1));
 	
 	//find the end of list
 	while(*(list.ordered_val+i)!='\0'){
