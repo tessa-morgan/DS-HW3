62117ead6c9e4db52ac9ed54d1121296cb323c1a
Tessa Morgan <tessa.morgan870@gmail.com>
2024-10-02 20:47:27 -0500

    12

--- RPC_System_client.c
+++ RPC_System_client.c
@@ -34,7 +34,9 @@ rpc_system_program_1(char *host)
 		printf("Enter 8 elements\n");
 		int input[8];
 		
-		scanf("%d %d %d %d %d %d %d %d", input[0], input[1], input[2], input[3], input[4], input[5], input[6], input[7]);
+		for (int i = 0; i < 8; i++) {
+			scanf("%d", &input[i]);
+		}
 
 		for (int i = 0; i < 8; i++) {
 			printf("%d,", input[i]);
